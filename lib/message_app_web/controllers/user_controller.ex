c7211f17259bf687aa957c0c8f31f8831e5c1453
Wasswa Samuel <latimerscope@gmail.com>
2020-11-10 10:53:20 +0200

    Add sign_in controller function

--- lib/message_app_web/controllers/user_controller.ex
+++ lib/message_app_web/controllers/user_controller.ex
@@ -40,4 +40,20 @@ defmodule MessageAppWeb.UserController do
       send_resp(conn, :no_content, "")
     end
   end
+
+  def sign_in(conn, %{"email" => email, "password" => password}) do
+    case MessageApp.Account.authenticate_user(email, password) do
+      {:ok, user} ->
+       conn
+        |> put_status(:ok)
+        |> put_view(MyAppWeb.UserView)
+        |> render("sign_in.json", user: user)
+
+      {:error, message} ->
+        conn
+        |> put_status(:unauthorized)
+        |> put_view(MyAppWeb.ErrorView)
+        |> render("401.json", message: message)
+    end
+  end
 end
